d5f8b5cc28592e7534a289bde53c505529b5ca88
asyncr <carrascoruizjesus@gmail.com>
2021-12-03 20:47:33 -0600

    Update

--- display.php
+++ display.php
@@ -9,12 +9,16 @@
 			<th scope="col">Email</th>
 			<th scope="col">Telefono</th>
 			<th scope="col">Puesto</th>
+			<th scope="col">Operaciones</th>
 		  </tr>
 		</thead>
 		';
 
 		$sql = "SELECT * FROM crud";
 		$result=mysqli_query($con,$sql);
+
+		// Contador diferente de la tabla de base de datos
+		$number = 1;		
 
 		while( $row = mysqli_fetch_assoc($result) ){
 			// Obtener los datos de las columnas
@@ -25,13 +29,17 @@
 			$place  = $row['place'];
 
 			$table.= '<tr>
-			<td scope="row"> '.$id.' </th>
+			<td scope="row"> '.$number.' </th>
 			<td>'.$nombre.'</td>
 			<td>'.$email.'</td>
 			<td>'.$mobile.'</td>
 			<td>'.$place.'</td>
-		  </tr>
-			';
+			<td>	
+				<botton class="btn btn-dark" onclick="GetDetails('.$id.')">Actualizar</botton>
+				<botton class="btn btn-danger" onclick="DeleteUser('.$id.')">Eliminar</botton>
+			</td>
+		  </tr>';
+		  $number++;
 		}
 		$table.='</table';
 		echo $table;
--- index.php
+++ index.php
@@ -7,7 +7,6 @@
   <title>CRUD - PHP</title>
   <!-- Boostrap 4.0 -->  
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-
 </head>
 <body>
 
@@ -63,7 +62,55 @@
     </div>
   </div>
 </div>
+  
+<!-- --------- Update ---------->
+  <!-- Modal -->
+<div class="modal fade" id="updateModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Actualizar Detalles</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <!-- FORMULARIO PARA REGISTRAR PERSONAS -->
+          
+        <div class="form-group">
+          <label for="updatename">Nombre</label>
+          <input type="text" class="form-control" id="updatename" aria-describedby="emailHelp" placeholder="Ingresa tu nombre">
+        </div>
 
+        <div class="form-group">
+          <label for="updateemail">Correo Electronico</label>
+          <input type="text" class="form-control" id="updateemail" aria-describedby="emailHelp" placeholder="Ingresa tu correo electronico">
+        </div>
+
+        <div class="form-group">
+          <label for="updatemobile">Telefono</label>
+          <input type="text" class="form-control" id="updatemobile" aria-describedby="emailHelp" placeholder="Ingresa tu telefono">
+        </div>
+
+        <div class="form-group">
+          <label for="updateplace">Puesto</label>
+          <input type="text" class="form-control" id="updateplace" aria-describedby="emailHelp" placeholder="Ingresa tu puesto">
+        </div>
+        ...
+
+      </div>
+
+      <!-- Boton Formulario Agregar Usuarios -->
+      <div class="modal-footer">
+        <button type="button" class="btn btn-dark"  onclick="GetDetails()">Actualizar</button>
+        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+        <input type="hidden" id="hiddendata"/>
+      </div>
+    </div>
+  </div>
+</div>
+
+  <!-- ----------Update ----------->
   <div class="container my-3">
     <h1 class="text-center">Operaciones CRUD Usando Boostrap y JQUERY</h1>
     <!-- BOTON AGREGAR PERSONAS -->
@@ -71,32 +118,25 @@
       Agregar Usuarios
     </button>
 
-
     <!-- Table -->
     <div id="displayDataTable">
-      
     </div>
-
   </div>
  
 
-
-
-
   <!-- Boostrap javascript -->
   <!-- <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" ></script> -->
-
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" ></script>
 
   <!-- Funciones JAVASCRIPT -->
   <script>
-    //Mostrar datos en la tabla
+    //Mostrar datos en la tabla en tiempo real
     $(document).ready(function(){
       displayData();
     });
-
+    //Obtener datos
     function displayData(){
       let displayData = "true";
       $.ajax({
@@ -118,7 +158,6 @@
       let emailAdd = $('#completeemail').val();
       let mobileAdd = $('#completemobile').val();
       let placeAdd = $('#completeplace').val();
-
       //AJAX
       $.ajax({
         url:'insert.php',
@@ -134,7 +173,32 @@
         }
       });
     }
-    
+    //Delete User
+    function DeleteUser(deleteid) {
+      $.ajax({
+        url:'delete.php',
+        type:'post',
+        data:{
+          deletesend:deleteid 
+        },
+        success:function(data,status){
+          displayData();
+        }
+      });
+    }
+      //Update User
+    function GetDetails(updateid){
+      $('#hiddendata').val(updateid);
+        $.post("update.php",{updateid:updateid},
+          function(data,status){
+            let userid = JSON.parse(data);
+            $('#updatename').val(userid.nombre);
+            $('#updateemail').val(userid.email);
+            $('#updatemobile').val(userid.mobile);
+            $('#updateplace').val(userid.place);
+          });
+        $('#updateModal').modal("show");
+    }
   </script>
 
 </body>
